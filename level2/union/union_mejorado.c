84bb15d3f092e27ebdb4f563a5a5c33cd42a5d5b
agomeza21 <gomezandresa03@gmail.com>
2026-05-04 18:58:32 +0200

    union mejorado

---
+++ level2/union/union_mejorado.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+	int i = 0;
+	int seen[256] = {0};
+	if (argc == 3)
+	{
+		while (argv[1][i])
+		{
+			if (seen[(int)argv[1][i]] == 0)
+			{
+				seen[(int)argv[1][i]] = 1;
+				write(1, &argv[1][i], 1);
+			}
+			i++;
+		}
+		int j = 0;
+		while (argv[2][j])
+		{
+			if (seen[(int)argv[2][j]] == 0)
+			{
+				seen[(int)argv[2][j]] = 1;
+				write(1, &argv[2][j], 1);
+			}
+			j++;
+		}
+	}
+	write(1, "\n", 1);
+}
